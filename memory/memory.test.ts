2b54412d0afbb0a3bd92d3ac64528557862b502c
Daniel Cardenas <danielcardeenas@gmail.com>
2024-08-30 12:59:25 -0700

    Update react, use Typescript

--- memory/memory.test.ts
+++ memory/memory.test.ts
@@ -1,49 +1,44 @@
-/*
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/**
+ * @jest-environment jsdom
  */
 
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from "@testing-library/react";
+import { describe, expect, test, jest, afterEach } from "@jest/globals";
 
-afterEach(function() {
+afterEach(function () {
   // Reload hook for every test
   jest.resetModules();
 });
 
-const getMemoryStatus = currentResult => ({
+const getMemoryStatus = (currentResult: {
+  deviceMemory: number;
+  totalJSHeapSize: number;
+  usedJSHeapSize: number;
+  jsHeapSizeLimit: number;
+  unsupported: boolean;
+}) => ({
   unsupported: false,
   deviceMemory: currentResult.deviceMemory,
   totalJSHeapSize: currentResult.totalJSHeapSize,
   usedJSHeapSize: currentResult.usedJSHeapSize,
-  jsHeapSizeLimit: currentResult.jsHeapSizeLimit
+  jsHeapSizeLimit: currentResult.jsHeapSizeLimit,
 });
 
-describe('useMemoryStatus', () => {
+describe("useMemoryStatus", () => {
   test(`should return "true" for unsupported case`, () => {
-    const { useMemoryStatus } = require('./');
+    const { useMemoryStatus } = require("./");
     const { result } = renderHook(() => useMemoryStatus());
 
     expect(result.current.unsupported).toBe(true);
   });
 
-  test('should return initialMemoryStatus for unsupported case', () => {
+  test("should return initialMemoryStatus for unsupported case", () => {
     const mockInitialMemoryStatus = {
-      deviceMemory: 4
+      deviceMemory: 4,
     };
     const { deviceMemory } = mockInitialMemoryStatus;
 
-    const { useMemoryStatus } = require('./');
+    const { useMemoryStatus } = require("./");
     const { result } = renderHook(() =>
       useMemoryStatus(mockInitialMemoryStatus)
     );
@@ -52,73 +47,79 @@ describe('useMemoryStatus', () => {
     expect(result.current.deviceMemory).toEqual(deviceMemory);
   });
 
-  test('should return mockMemory status', () => {
+  test("should return mockMemory status", () => {
     const mockMemoryStatus = {
       deviceMemory: 4,
       totalJSHeapSize: 60,
       usedJSHeapSize: 40,
-      jsHeapSizeLimit: 50
+      jsHeapSizeLimit: 50,
     };
 
+    // @ts-ignore
     global.navigator.deviceMemory = mockMemoryStatus.deviceMemory;
 
+    // @ts-ignore
     global.window.performance.memory = {
       totalJSHeapSize: mockMemoryStatus.totalJSHeapSize,
       usedJSHeapSize: mockMemoryStatus.usedJSHeapSize,
-      jsHeapSizeLimit: mockMemoryStatus.jsHeapSizeLimit
+      jsHeapSizeLimit: mockMemoryStatus.jsHeapSizeLimit,
     };
 
-    const { useMemoryStatus } = require('./');
+    const { useMemoryStatus } = require("./");
     const { result } = renderHook(() => useMemoryStatus());
 
     expect(getMemoryStatus(result.current)).toEqual({
       ...mockMemoryStatus,
-      unsupported: false
+      unsupported: false,
     });
   });
 
-  test('should return mockMemory status without performance memory data', () => {
+  test("should return mockMemory status without performance memory data", () => {
     const mockMemoryStatus = {
-      deviceMemory: 4
+      deviceMemory: 4,
     };
 
+    // @ts-ignore
     global.navigator.deviceMemory = mockMemoryStatus.deviceMemory;
+    // @ts-ignore
     delete global.window.performance.memory;
 
-    const { useMemoryStatus } = require('./');
+    const { useMemoryStatus } = require("./");
     const { result } = renderHook(() => useMemoryStatus());
 
     expect(result.current.deviceMemory).toEqual(mockMemoryStatus.deviceMemory);
     expect(result.current.unsupported).toEqual(false);
   });
 
-  test('should not return initialMemoryStatus for supported case', () => {
+  test("should not return initialMemoryStatus for supported case", () => {
     const mockMemoryStatus = {
       deviceMemory: 4,
       totalJSHeapSize: 60,
       usedJSHeapSize: 40,
-      jsHeapSizeLimit: 50
+      jsHeapSizeLimit: 50,
     };
     const mockInitialMemoryStatus = {
-      deviceMemory: 4
+      deviceMemory: 4,
     };
 
+    // @ts-ignore
     global.navigator.deviceMemory = mockMemoryStatus.deviceMemory;
 
+    // @ts-ignore
     global.window.performance.memory = {
       totalJSHeapSize: mockMemoryStatus.totalJSHeapSize,
       usedJSHeapSize: mockMemoryStatus.usedJSHeapSize,
-      jsHeapSizeLimit: mockMemoryStatus.jsHeapSizeLimit
+      jsHeapSizeLimit: mockMemoryStatus.jsHeapSizeLimit,
     };
 
-    const { useMemoryStatus } = require('./');
+    const { useMemoryStatus } = require("./");
     const { result } = renderHook(() =>
       useMemoryStatus(mockInitialMemoryStatus)
     );
 
     expect(getMemoryStatus(result.current)).toEqual({
       ...mockMemoryStatus,
-      unsupported: false
+      unsupported: false,
     });
   });
 });
--- save-data/save-data.test.js
+++ save-data/save-data.test.js
@@ -1,20 +1,8 @@
-/*
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/**
+ * @jest-environment jsdom
  */
 
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react'
 
 afterEach(function() {
   // Reload hook for every test
@@ -23,7 +11,7 @@ afterEach(function() {
 
 describe('useSaveData', () => {
   test(`should return "true" for unsupported case`, () => {
-    const { useSaveData } = require('./');
+    const { useSaveData } = require('./save-data');
     const { result } = renderHook(() => useSaveData());
 
     expect(result.current.unsupported).toBe(true);
@@ -32,7 +20,7 @@ describe('useSaveData', () => {
 
   test('should return initialSaveData for unsupported case', () => {
     const initialSaveData = true;
-    const { useSaveData } = require('./');
+    const { useSaveData } = require('./save-data');
     const { result } = renderHook(() => useSaveData(initialSaveData));
 
     expect(result.current.unsupported).toBe(true);
@@ -43,7 +31,7 @@ describe('useSaveData', () => {
     global.navigator.connection = {
       saveData: true
     };
-    const { useSaveData } = require('./');
+    const { useSaveData } = require('./save-data');
     const { result } = renderHook(() => useSaveData());
 
     expect(result.current.unsupported).toBe(false);
@@ -54,7 +42,7 @@ describe('useSaveData', () => {
     global.navigator.connection = {
       saveData: false
     };
-    const { useSaveData } = require('./');
+    const { useSaveData } = require('./save-data');
     const { result } = renderHook(() => useSaveData());
 
     expect(result.current.unsupported).toBe(false);
@@ -66,7 +54,7 @@ describe('useSaveData', () => {
     global.navigator.connection = {
       saveData: true
     };
-    const { useSaveData } = require('./');
+    const { useSaveData } = require('./save-data');
     const { result } = renderHook(() => useSaveData(initialSaveData));
 
     expect(result.current.unsupported).toBe(false);
